c97b6399bc160def03468981cfff0efa70df123f
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-03-24 14:46:28 -0300

    05.04 - Validacoes finais

--- lib/screens/transferencia/ultimas.dart
+++ lib/screens/transferencia/ultimas.dart
@@ -15,18 +15,31 @@ class UltimasTransferencias extends StatelessWidget {
       ),
       Consumer<Transferencias>(
         builder: (context, transferencias, child) {
+          final _ultimasTransferencias =
+              transferencias.transferencias.reversed.toList();
+          final int _quantidade = transferencias.transferencias.length;
+          int tamanho = 2;
+
+          if (_quantidade == 0) {
+            return SemTransferenciaCadastrada();
+          }
+
+          if (_quantidade < 3) {
+            tamanho = _quantidade;
+          }
+
           return ListView.builder(
             padding: EdgeInsets.all(8),
-            itemCount: 2,
+            itemCount: tamanho,
             shrinkWrap: true,
             itemBuilder: (context, indice) {
-              return ItemTransferencia(transferencias.transferencias[indice]);
+              return ItemTransferencia(_ultimasTransferencias[indice]);
             },
           );
         },
       ),
       RaisedButton(
-        child: Text('Transferências'),
+        child: Text('Ver todas as transferências'),
         color: Colors.green,
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -35,5 +48,21 @@ class UltimasTransferencias extends StatelessWidget {
         },
       )
     ]);
+  }
+}
+
+class SemTransferenciaCadastrada extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(40),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Você ainda não realizou nenhuma transferências.',
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
   }
 }
